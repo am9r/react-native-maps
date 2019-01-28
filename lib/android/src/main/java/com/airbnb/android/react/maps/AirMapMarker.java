c77609858b4ddd38c94e734c12c6f28122debbe9
Amor Lisic <amor.lisic@beka-software.at>
2019-01-28 14:28:34 +0100

    fix chaging icon to default marker

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarker.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarker.java
@@ -279,15 +279,13 @@ public class AirMapMarker extends AirMapFeature {
   }
 
   public void updateMarkerIcon() {
-    if (!hasViewChanges) return;
+    if (marker == null) return;
 
     if (!hasCustomMarkerView) {
       // No more updates for this, as it's a simple icon
       hasViewChanges = false;
     }
-    if (marker != null) {
       marker.setIcon(getIcon());
-    }
   }
 
   public LatLng interpolate(float fraction, LatLng a, LatLng b) {
